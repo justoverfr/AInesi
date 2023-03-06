4414365eef451acbb53b1daca13a6306df0986eb
leo <leoyng95@gmail.com>
2023-03-06 09:28:06 +0100

    pipeline test

--- pipeline.py
+++ pipeline.py
@@ -1,19 +1,21 @@
 from transformers import pipeline 
 
+'''#analyse de sentiment
 classi = pipeline("sentiment-analysis")
 resultat = classi("I dont like my friend")
 
 print(resultat)
 
+#traduction
 trans= pipeline("translation_en_to_fr")
 transit = trans("My name is LEO, I have 19 years ols today !")
 
 print(transit)
 
-
+#question réponses
 classifier = pipeline("question-answering")
 question = "how old are you?"       
 context = "My name is LEO, I have 19 years old today !"
 res = classifier (question = question, context = context)
 
-print(res)
+print(res)'''
