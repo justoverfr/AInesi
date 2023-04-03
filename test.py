a1cabce2c220937c4c221b17f8eb39b1524e01e8
justoverfr <jeanjuste.lefebvre@gmail.com>
2023-04-04 00:11:52 +0200

    ok

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import openai
 
-openai.api_key = 
+openai.api_key = "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"
+
+response = openai.Completion.create(engine="text-davinci-003", prompt="This is a test", temperature=0.6,
+                                    max_tokens=5,
+                                    top_p=1,
+                                    frequency_penalty=0,
+                                    presence_penalty=0.6,
+                                    stop=["\n", " Human:", " AI:"])
+
+print(response)
